b9ba7dff28050731f93c61f7f48901c32a288a32
e.s <esolakster@gmail.com>
2020-08-10 23:03:40 +0200

    Projects

---
+++ SamuraiApp.Data/SamuraiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SamutaiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiDbContext: DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+        
+    }
+}
---
+++ SamutaiApp.Domain/Samurai.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SamutaiApp.Domain
+{
+    public class Samurai
+    {
+        public Samurai()
+        {
+            Quotes = new List<Quote>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Quote> Quotes { get; set; }
+        public Clan Clan { get; set; }
+    }
+
+    public class Clan
+    {
+        public int Id { get; set; }
+        public string ClanName { get; set; }
+
+    }
+
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public Samurai Samurai { get; set; }
+        public int SamuraiId { get; set; }
+    }
+}
